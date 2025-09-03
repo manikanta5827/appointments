0489c3f644a7f99cae0a31802a0cddce29dcfaf4
Manikanta Thummuri <postbox5827@gmail.com>
2025-09-03 12:42:24 +0530

    feat: add enum

---
+++ enum/bookingStatusEnum.js
@@ -0,0 +1,11 @@
+
+
+export const UserRole = Object.freeze({
+    STUDENT: "student",
+    PROFESSOR: "professor",
+  });
+  
+export const AppointmentStatus = Object.freeze({
+    BOOKED: "booked",
+    CANCELLED: "cancelled",
+  });
